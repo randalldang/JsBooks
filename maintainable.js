ca69a3725515355bfd38bf97ee0439800019240c
Randall <yun.feng.dang@iuo.citi.com>
2013-12-30 16:51:42 +0800

    done

--- maintainable.js
+++ maintainable.js
@@ -1,2 +1,13 @@
 //Blank line after for/while/if
+
 // Two indedents before the break line
+
+// Declare any local viriable at the top of the function
+
+//Combine all local viriable with one var
+
+// Avoid == and !==, try to use === and !===. Because 5 == '5'(valueof, toString)
+
+//Avoid to use primitive wrapper types(String, Boolean, Number)
+
+//Check property: prop in obj
